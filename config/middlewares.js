4cc0c34a28602ddd091fbd8d0d39d15a999a2309
razuahmedjoy <rjjoybzs420@gmail.com>
2024-01-23 18:20:05 +0600

    updated middleware file

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,20 +6,10 @@ module.exports = [
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'res.cloudinary.com',
-          ],
-          upgradeInsecureRequests: null,
+          'img-src': ["'self'", 'data:', 'strapi.io', 'res.cloudinary.com/'],
         },
-      },
+      }
     },
   },
 
